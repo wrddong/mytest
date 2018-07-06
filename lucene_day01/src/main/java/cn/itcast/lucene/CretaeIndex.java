e16c2c69719bb9047b514fb370669eb44ba634a3
water <wrd421223@126.com>
2018-07-06 22:55:40 +0800

    员1修改

--- lucene_day01/src/main/java/cn/itcast/lucene/CretaeIndex.java
+++ lucene_day01/src/main/java/cn/itcast/lucene/CretaeIndex.java
@@ -10,9 +10,13 @@ public class CretaeIndex {
     @Test
     public void testCreateIndex()throws Exception{
         //指定索引库目录
-
         //创建索引库核心写对象
         IndexWriter indexWriter = new IndexWriter();
     }
-
+ @Test
+    public void testCreateIndex1()throws Exception{
+        //指定索引库目录
+        //创建索引库核心写对象
+        IndexWriter indexWriter = new IndexWriter();
+    }
 }
