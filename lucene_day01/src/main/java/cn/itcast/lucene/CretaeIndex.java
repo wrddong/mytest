327f7f6363f8e4331d0d3e00373cfb5a0f959058
water <wrd421223@126.com>
2018-07-06 21:45:03 +0800

    111

---
+++ lucene_day01/src/main/java/cn/itcast/lucene/CretaeIndex.java
@@ -0,0 +1,18 @@
+package cn.itcast.lucene;
+
+import org.junit.Test;
+
+/**
+ * 创建索引库
+ */
+public class CretaeIndex {
+
+    @Test
+    public void testCreateIndex()throws Exception{
+        //指定索引库目录
+
+        //创建索引库核心写对象
+        IndexWriter indexWriter = new IndexWriter();
+    }
+
+}
